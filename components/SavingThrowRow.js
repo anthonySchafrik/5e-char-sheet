593b902641689d0f0cdebf9dfe726b31fc1e1ef0
Anthony Schafrik <anthonyschafrik@gmail.com>
2020-01-26 19:54:02 -0500

    Set up saving thorws to add grid in to it

--- components/SavingThrowRow.js
+++ components/SavingThrowRow.js
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet, TextInput } from 'react-native';
+import { Col, Row, Grid } from 'react-native-easy-grid';
+
+import Colors from '../Colors';
 
 const SavingThrowRow = ({ text, handler }) => {
   const [checked, handleChecked] = useState(false);
@@ -7,13 +10,18 @@ const SavingThrowRow = ({ text, handler }) => {
   return (
     <View style={styles.container}>
       <View
-        onPress={() => handleChecked(!checked)}
-        style={{ backgroundColor: checked ? 'black' : 'white' }}
-      ></View>
+        style={{
+          ...styles.checkedCircle,
+          backgroundColor: checked ? 'black' : Colors.underLine
+        }}
+      >
+        <Text onPress={() => handleChecked(!checked)} />
+      </View>
       <TextInput
         style={{ paddingLeft: 6 }}
         placeholder="Mult"
         placeholderTextColor="black"
+        style={styles.styledInput}
       />
       <Text>{text}</Text>
     </View>
@@ -23,7 +31,19 @@ const SavingThrowRow = ({ text, handler }) => {
 const styles = StyleSheet.create({
   container: {
     flexDirection: 'row',
-    justifyContent: 'space-around'
+    justifyContent: 'space-around',
+    marginVertical: 5
+  },
+  checkedCircle: {
+    height: 25,
+    width: 25,
+    borderColor: '#323232',
+    borderWidth: 1,
+    borderRadius: 25
+  },
+  styledInput: {
+    borderColor: Colors.underLine,
+    borderBottomWidth: 1
   }
 });
 
