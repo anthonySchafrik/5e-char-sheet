6bf151c3f0be7a659a961329fcd1933dbe1b2f5b
Anthony Schafrik <anthonyschafrik@gmail.com>
2020-02-07 19:31:51 -0500

    Added in the ui

--- screens/SelectedCharacter/Equipment.js
+++ screens/SelectedCharacter/Equipment.js
@@ -1,12 +1,50 @@
-import React, { useEffect } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState, useEffect, useReducer } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  ScrollView,
+  KeyboardAvoidingView
+} from 'react-native';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+
+import { updateSelectedCharacter } from '../../actions/characters';
 
 import Menu from '../../components/Menu';
 import Colors from '../../Colors';
 
-const Equipment = ({ treasure, equipment, navigation }) => {
+const equipmentReducer = (state, action) => {
+  const { type, payload } = action;
+  const { key, value } = payload;
+
+  switch (type) {
+    case 'update':
+      return {
+        ...state,
+        [key]: value
+      };
+    default:
+      throw new Error();
+  }
+};
+
+const Equipment = ({
+  treasure,
+  equipment,
+  navigation,
+  updateSelectedCharacter
+}) => {
+  const [updatedTreasure, handleUpdatedTreasure] = useState(treasure);
+
+  const [updatedEquipment, equipmentDispatch] = useReducer(
+    equipmentReducer,
+    equipment
+  );
+
+  const { cp, sp, ep, gp, pp, text } = updatedEquipment;
+
   useEffect(() => {
     return async () => {
       try {
@@ -20,9 +58,63 @@ const Equipment = ({ treasure, equipment, navigation }) => {
 
   return (
     <View style={styles.screen}>
-      <Menu navigation={navigation} />
-      <Text>{JSON.stringify({ treasure })}</Text>
-      <Text>{JSON.stringify({ equipment })}</Text>
+      <View style={styles.styledMenu}>
+        <Menu navigation={navigation} />
+      </View>
+
+      <View style={styles.topContainer}>
+        <Text style={styles.styledText}>Treasure</Text>
+        <ScrollView style={{ width: '100%' }}>
+          <TextInput
+            multiline={true}
+            style={styles.styledTextField}
+            value={updatedTreasure}
+            onChangeText={text => handleUpdatedTreasure(text)}
+            onEndEditing={() =>
+              updateSelectedCharacter({
+                key: 'treasure',
+                value: updatedTreasure
+              })
+            }
+          />
+        </ScrollView>
+      </View>
+
+      <ScrollView>
+        <View style={{ ...styles.row, ...styles.rowContainer }}>
+          <View>
+            <View style={styles.row}>
+              <Text>Cp</Text>
+              <TextInput value={cp} style={styles.rowInput} />
+            </View>
+
+            <View style={styles.row}>
+              <Text>Sp</Text>
+              <TextInput value={sp} style={styles.rowInput} />
+            </View>
+
+            <View style={styles.row}>
+              <Text>Ep</Text>
+              <TextInput value={ep} style={styles.rowInput} />
+            </View>
+
+            <View style={styles.row}>
+              <Text>Gp</Text>
+              <TextInput value={gp} style={styles.rowInput} />
+            </View>
+
+            <View style={styles.row}>
+              <Text>Pp</Text>
+              <TextInput value={pp} style={styles.rowInput} />
+            </View>
+          </View>
+          <TextInput
+            value={text}
+            multiline={true}
+            style={styles.equipmentTextField}
+          />
+        </View>
+      </ScrollView>
     </View>
   );
 };
@@ -33,7 +125,7 @@ const mapStateToProps = state => {
 };
 
 const mapDispatchToProp = dispatch => {
-  return bindActionCreators({}, dispatch);
+  return bindActionCreators({ updateSelectedCharacter }, dispatch);
 };
 
 const styles = StyleSheet.create({
@@ -41,7 +133,53 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: Colors.background,
     paddingTop: 30,
-    justifyContent: 'space-around'
+    alignItems: 'center'
+  },
+  topContainer: {
+    width: '95%',
+    height: 200,
+    alignItems: 'center',
+    marginBottom: 60
+  },
+  styledText: {
+    color: Colors.font,
+    marginVertical: 5
+  },
+  styledTextField: {
+    color: Colors.font,
+    borderWidth: 1,
+    borderColor: Colors.secondary,
+    padding: 5,
+    width: '95%'
+  },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  rowInput: {
+    color: Colors.font,
+    borderBottomWidth: 1,
+    borderBottomColor: Colors.secondary,
+    paddingLeft: 5
+  },
+  rowContainer: {
+    justifyContent: 'space-around',
+    alignItems: 'flex-start',
+    borderColor: Colors.secondary,
+    borderWidth: 1,
+    width: '95%',
+    height: 160
+  },
+  styledMenu: {
+    alignSelf: 'flex-start',
+    paddingLeft: 10,
+    paddingTop: 10
+  },
+  equipmentTextField: {
+    color: Colors.font,
+    borderBottomWidth: 1,
+    borderBottomColor: Colors.secondary,
+    width: '60%'
   }
 });
 
